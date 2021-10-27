c83577655e151ba8f5891d1fbc79bdd900002e7a
luludeolive <lucianaoliveirabacelar@gmail.com>
2021-10-27 15:22:02 -0300

    finish app

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let saveNumber = count + " - "
+    saveEl.textContent += saveNumber
+    count = 0
+    countEl.textContent = count
+}
